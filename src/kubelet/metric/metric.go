a220bb602958e92e330f175a3c74c01f99bfa1c5
Magdalena Pajda <mpajda@newrelic.com>
2018-01-24 18:30:10 +0100

    Adding logic for fetching node data from kubelet

--- src/kubelet/metric/metric.go
+++ src/kubelet/metric/metric.go
@@ -14,6 +14,17 @@ import (
 // Summary represents list of required data from /stats/summary endpoint
 type Summary struct {
 	Pods []Pod `json:"pods"`
+	Node Node  `json:"node"`
+}
+
+// Node represents all required node data from kubelet endpoint
+type Node struct {
+	Name    string  `json:"nodeName"`
+	CPU     CPU     `json:"cpu"`
+	Memory  Memory  `json:"memory"`
+	Network Network `json:"network"`
+	Fs      Fs      `json:"fs"`
+	Runtime Runtime `json:"runtime"`
 }
 
 // Pod represents all required pod and container data for kubelet integration
@@ -29,7 +40,7 @@ type PodRef struct {
 	Namespace string `json:"namespace"`
 }
 
-// Network represents network data of pod
+// Network represents network data of pod or node
 type Network struct {
 	RxBytes  int `json:"rxBytes"`
 	TxBytes  int `json:"txBytes"`
@@ -44,14 +55,45 @@ type Container struct {
 	Memory Memory `json:"memory"`
 }
 
-// CPU represents core CPU usage data of container
+// CPU represents core CPU usage data of container or node
 type CPU struct {
-	UsageNanoCores int `json:"usageNanoCores"`
+	UsageNanoCores       int `json:"usageNanoCores"`
+	UsageCoreNanoSeconds int `json:"usageCoreNanoSeconds"`
 }
 
-// Memory represents memory usage data of container
+// Memory represents memory usage data of container or node
 type Memory struct {
-	UsageBytes int `json:"usageBytes"`
+	UsageBytes      int `json:"usageBytes"`
+	AvailableBytes  int `json:"availableBytes"`
+	WorkingSetBytes int `json:"workingSetBytes"`
+	RssBytes        int `json:"rssBytes"`
+	PageFaults      int `json:"pageFaults"`
+	MajorPageFaults int `json:"majorPageFaults"`
+}
+
+// Fs represents file system data of a node
+type Fs struct {
+	AvailableBytes int `json:"availableBytes"`
+	CapacityBytes  int `json:"capacityBytes"`
+	UsedBytes      int `json:"usedBytes"`
+	InodesFree     int `json:"inodesFree"`
+	Inodes         int `json:"inodes"`
+	InodesUsed     int `json:"inodesUsed"`
+}
+
+// Runtime ...
+type Runtime struct {
+	ImageFS ImageFS `json:"imageFs"`
+}
+
+// ImageFS represents image filesystem usage data of a node
+type ImageFS struct {
+	AvailableBytes int `json:"availableBytes"`
+	CapacityBytes  int `json:"capacityBytes"`
+	UsedBytes      int `json:"usedBytes"`
+	InodesFree     int `json:"inodesFree"`
+	Inodes         int `json:"inodes"`
+	InodesUsed     int `json:"inodesUsed"`
 }
 
 // GetMetricsData calls kubelet /stats/summary endpoint and returns unmarshalled response
@@ -77,12 +119,52 @@ func GetMetricsData(netClient *http.Client, URL string) (*Summary, error) {
 
 }
 
-// GroupStatsSummary groups specific data for pods and containers
+// GroupStatsSummary groups specific data for pods, containers and node
 func GroupStatsSummary(statsSummary *Summary) (definition.RawGroups, []error) {
 	var errs []error
 	g := definition.RawGroups{
 		"pod":       {},
 		"container": {},
+		"node":      {},
+	}
+
+	node := statsSummary.Node
+	if node.Name != "" {
+		rawEntityID := fmt.Sprintf("%v", statsSummary.Node.Name)
+		nodeData := definition.RawMetrics{
+			"nodeName": node.Name,
+			// CPU
+			"usageNanoCores":       node.CPU.UsageNanoCores,
+			"usageCoreNanoSeconds": node.CPU.UsageCoreNanoSeconds,
+			// Memory
+			"memoryUsageBytes":      node.Memory.UsageBytes,
+			"memoryAvailableBytes":  node.Memory.AvailableBytes,
+			"memoryWorkingSetBytes": node.Memory.WorkingSetBytes,
+			"memoryRssBytes":        node.Memory.RssBytes,
+			"memoryPageFaults":      node.Memory.PageFaults,
+			"memoryMajorPageFaults": node.Memory.PageFaults,
+			// Network
+			"rxBytes": node.Network.RxBytes,
+			"txBytes": node.Network.TxBytes,
+			"errors":  node.Network.RxErrors + node.Network.TxErrors,
+			// Fs
+			"fsAvailableBytes": node.Fs.AvailableBytes,
+			"fsCapacityBytes":  node.Fs.CapacityBytes,
+			"fsUsedBytes":      node.Fs.UsedBytes,
+			"fsInodesFree":     node.Fs.InodesFree,
+			"fsInodes":         node.Fs.Inodes,
+			"fsInodesUsed":     node.Fs.InodesUsed,
+			// Runtime
+			"runtimeAvailableBytes": node.Runtime.ImageFS.AvailableBytes,
+			"runtimeCapacityBytes":  node.Runtime.ImageFS.CapacityBytes,
+			"runtimeUsedBytes":      node.Runtime.ImageFS.UsedBytes,
+			"runtimeInodesFree":     node.Runtime.ImageFS.InodesFree,
+			"runtimeInodes":         node.Runtime.ImageFS.Inodes,
+			"runtimeInodesUsed":     node.Runtime.ImageFS.InodesUsed,
+		}
+		g["node"][rawEntityID] = nodeData
+	} else {
+		errs = append(errs, fmt.Errorf("empty node identifier, fetching pod data skipped"))
 	}
 
 	for _, pod := range statsSummary.Pods {
