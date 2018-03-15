c5c918cf3c78fd8db87d3a1848feca1e7277c2d0
Magdalena Pajda <mpajda@newrelic.com>
2018-03-15 17:28:38 +0100

    Removing unnecessary functions

--- src/metric/metric.go
+++ src/metric/metric.go
@@ -4,51 +4,10 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/newrelic/infra-integrations-beta/integrations/kubernetes/src/config"
 	"github.com/newrelic/infra-integrations-beta/integrations/kubernetes/src/definition"
-	ksmMetric "github.com/newrelic/infra-integrations-beta/integrations/kubernetes/src/ksm/metric"
-	kubeletMetric "github.com/newrelic/infra-integrations-beta/integrations/kubernetes/src/kubelet/metric"
 	"github.com/newrelic/infra-integrations-sdk/metric"
 	"github.com/newrelic/infra-integrations-sdk/sdk"
 )
-
-// NamespaceFetcher fetches the namespace from the provided RawGroups, for the given groupLabel and entityId
-type NamespaceFetcher func(groupLabel, entityID string, groups definition.RawGroups) (string, error)
-
-// MultipleNamespaceFetcher asks multiple NamespaceFetcher instances for the namespace. It queries the NamespaceFetchers
-// in the provided order and returns the namespace from the first invocation that does not return error. If all the
-// invocations fail, this function returns "_unknown" namespace and an error instance.
-func MultipleNamespaceFetcher(groupLabel, entityID string, groups definition.RawGroups, fetchers ...NamespaceFetcher) (string, error) {
-
-	errors := make([]string, 0)
-
-	for _, fetcher := range fetchers {
-		ns, err := fetcher(groupLabel, entityID, groups)
-		if err == nil {
-			return ns, nil
-		}
-		errors = append(errors, err.Error())
-	}
-
-	return config.UnknownNamespace, fmt.Errorf("error fetching namespace: %s", strings.Join(errors, ", "))
-}
-
-// K8sMetricSetEntityTypeGuesser guesses the Entity Type given a group name, entity Id and a namespace fetcher function
-func K8sMetricSetEntityTypeGuesser(clusterName, groupLabel, entityID string, groups definition.RawGroups) (string, error) {
-	var actualGroupLabel string
-	switch groupLabel {
-	case "namespace", "node":
-		return fmt.Sprintf("k8s:%s:%s", clusterName, groupLabel), nil
-	case "container":
-		actualGroupLabel = "pod"
-	default:
-		actualGroupLabel = groupLabel
-	}
-	ns, err := MultipleNamespaceFetcher(groupLabel, entityID, groups,
-		kubeletMetric.KubeletNamespaceFetcher,
-		ksmMetric.KsmNamespaceFetcher)
-	return fmt.Sprintf("k8s:%s:%s:%s", clusterName, ns, actualGroupLabel), err
-}
 
 // K8sMetricSetTypeGuesser is the metric set type guesser for k8s integrations.
 func K8sMetricSetTypeGuesser(_, groupLabel, _ string, _ definition.RawGroups) (string, error) {
